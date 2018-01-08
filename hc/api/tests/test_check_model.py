84d2c487f69726e45d9548309f7875a37e1e5ceb
Allan Ibutiti <allan1mwangi@gmail.com>
2018-01-08 11:08:15 +0300

    Upgrading Django to V1.11 (#6)
    * Added Procfile. Edited requirements.txt
    
    * Fix settings.py for heroku deployment
    
    * Fix: Added whitenoise to serve static files
    
    * [ADD] TravisCI Slack notifications (#1)
    
    * [ADD] TravisCI Slack notifications
    
    * [ADD] coveralls integration config
    
    * [UPDATE] psycopg2 version on requirements.txt
    
    * [ADD] CircleCI configuration
    
    * [UPDATE] README with integration badges
    
    * [FIX] removed unnecessary config on .coveralls.yml
    
    * [UPDATE] python build version .travis.yml
    
    * [FIX] CircleCI configuration
    
    * [FIX] CircleCI config added coveralls repo_token
    
    * [UPDATE] Integration badges
    
    * Added HEROKU_DEPLOYMENT env variable to populate db settings in heroku environment
    
    * Added missing package mock to requirements.txt
    
    * Added whitenoise to middleware list in settings.py
    
    * Added collectstatic script to .travis.yml
    
    * Added --noinput option to travis.yml to let collectstatic script run without user confirmation
    
    * Upgrade project to django 1.11. Fix deprecated method in hc/api/tests/test_check_model.py, change django version to 1.11 in requirements.txt file
    
    * Upgrade project to django 1.11. Fix deprecated method in hc/api/tests/test_check_model.py, change django version to 1.11 in requirements.txt file
    
    * Added allowed hosts into settings.py file
    
    * Added allowed hosts to settings.py file
    
    * Fixed allowed hosts in settings.py file

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -11,8 +11,8 @@ class CheckModelTestCase(TestCase):
         check = Check()
 
         check.tags = " foo  bar "
-        self.assertEquals(check.tags_list(), ["foo", "bar"])
-        ### Repeat above test for when check is an empty string
+        self.assertEqual(check.tags_list(), ["foo", "bar"])
+        # Repeat above test for when check is an empty string
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -23,7 +23,7 @@ class CheckModelTestCase(TestCase):
         self.assertTrue(check.in_grace_period())
         self.assertEqual(check.get_status(), "up")
 
-        ### The above 2 asserts fail. Make them pass
+        # The above 2 asserts fail. Make them pass
 
     def test_paused_check_is_not_in_grace_period(self):
         check = Check()
@@ -35,4 +35,4 @@ class CheckModelTestCase(TestCase):
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
 
-    ### Test that when a new check is created, it is not in the grace period
+    # Test that when a new check is created, it is not in the grace period
