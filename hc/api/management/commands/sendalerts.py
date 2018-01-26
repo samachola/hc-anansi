f6b2383c9fd229c0567717bf9df8f42c1035b0ef
Sam Achola <sam.achola@live.com>
2018-01-26 10:56:08 +0300

    [#154658296:Chore] Fixed nag interval alert conflict

--- hc/api/management/commands/sendalerts.py
+++ hc/api/management/commands/sendalerts.py
@@ -44,7 +44,11 @@ class Command(BaseCommand):
         # Save the new status. If sendalerts crashes,
         # it won't process this check again.
         check.status = check.get_status()
+        if check.status == "down":
+            check.nag_status = True
+            
         check.save()
+            
 
         tmpl = "\nSending alert, status=%s, code=%s\n"
         self.stdout.write(tmpl % (check.status, check.code))
